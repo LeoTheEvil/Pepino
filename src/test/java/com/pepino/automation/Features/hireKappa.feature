d6ce2690ea81f02a8f493d63acab47b6dd711e53
LeoTheEvil <iceninjashidomaru@gmail.com>
2024-11-30 15:06:03 -0300

    SEVERE: Exception while executing pickle

--- src/test/java/com/pepino/automation/Features/hireKappa.feature
+++ src/test/java/com/pepino/automation/Features/hireKappa.feature
@@ -2,6 +2,8 @@ Feature: Hire_Kappa
 
   Scenario: Hire a new kappa
 
-    Given un kappa llamado Kappasuke de rango Kappitan y clase Samurai
-    When el usuario hace clic en Post
+    Given un kappa llamado Kappasuke
+    And de rango Kappitan
+    And de clase Samurai
+    When el usuario hace un Post
     Then el kappa es contratado
--- src/test/java/com/pepino/automation/Steps/hireKappaSteps.java
+++ src/test/java/com/pepino/automation/Steps/hireKappaSteps.java
@@ -1,20 +1,39 @@
 package com.pepino.automation.Steps;
 
+import com.pepino.automation.Modelo.Kappa;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import org.springframework.http.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.port;
+import static org.hamcrest.Matchers.equalTo;
 
 public class hireKappaSteps {
-    @Given("un kappa llamado Kappasuke de rango Kappitan y clase Samurai")
-    public void unKappaLlamadoKappasukeRangoKappitanClaseSamurai() {
-
+    Kappa kappa = new Kappa();
+    Long id;
+    @Given("un kappa llamado Kappasuke")
+    public void unKappaLlamadoKappasuke() {
+        kappa.setNombre("Kappasuke");
     }
-    @When("el usuario hace clic en Post")
-    public void usuarioClicPost() {
-
+    @Given("de rango Kappitan")
+    public void rangoKappitan()
+    {
+        kappa.setRango("Kappitan");
+    }
+    @Given("de clase Samurai")
+    public void claseSamurai() {
+        kappa.setClase("Samurai");
+    }
+    @When("el usuario hace un Post")
+    public void usuarioHacePost() {
+        id=given().port(port).body(kappa).contentType(MediaType.APPLICATION_JSON.toString())
+        .accept(MediaType.APPLICATION_JSON.toString()).when().post("/api/kappa").then().statusCode(201)
+        .extract().jsonPath().getObject("id",Long.class);
     }
     @Then("el kappa es contratado")
     public void kappaContratado() {
-
+        given().port(port).when().get("/api/kappa/"+id).then().body("id", equalTo(id));
     }
 }
